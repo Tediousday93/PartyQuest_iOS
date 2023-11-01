671e0717d86c2a9198f00179ab240aa19970d94f
HarryHyeon <hharry0728@gmail.com>
2023-11-01 17:34:06 +0900

    feat: SignUpViewModel에서 Input, Output, transform 구현

--- PartyQuest_iOS/PartyQuest_iOS/Presentation/Scenes/SignUpView/SignUpViewModel.swift
+++ PartyQuest_iOS/PartyQuest_iOS/Presentation/Scenes/SignUpView/SignUpViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 final class SignUpViewModel {
     private let coordinator: SignUpCoordinator
@@ -17,15 +19,46 @@ final class SignUpViewModel {
 
 extension SignUpViewModel: ViewModelType {
     struct Input {
-
+        let email: Observable<String>
+        let password: Observable<String>
+        let birthDate: Observable<String>
+        let nickName: Observable<String>
     }
     
     struct Output {
-
+        let isEmailValid: Driver<Bool>
+        let isPasswordValid: Driver<Bool>
+        let isNickNameValid: Driver<Bool>
+        let isEnableSignUpButton: Driver<Bool>
     }
     
     func transform(_ input: Input) -> Output {
+        let email = input.email
+            .map { text in
+                return text.isValidEmail()
+            }
+            .asDriver(onErrorJustReturn: false)
         
-        return Output()
+        let password = input.password
+            .map { text in
+                return text.isValidPassword()
+            }
+            .asDriver(onErrorJustReturn: false)
+        
+        let nickName = input.nickName
+            .map { text in
+                return text.isValidNickname()
+            }
+            .asDriver(onErrorJustReturn: false)
+        
+        let isEnableSignUpButton = Driver.combineLatest(email, password, nickName)
+            .map { isEmailValidate, isPasswordValidate, isNickName in
+                return isEmailValidate && isPasswordValidate && isNickName
+            }
+        
+        return Output(isEmailValid: email,
+                      isPasswordValid: password,
+                      isNickNameValid: nickName,
+                      isEnableSignUpButton: isEnableSignUpButton)
     }
 }

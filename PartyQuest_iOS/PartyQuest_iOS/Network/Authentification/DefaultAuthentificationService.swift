845d31e2de8c236d5433e7254cde086a45f605f2
ROWAN <kimkj93@naver.com>
2023-11-03 14:56:05 +0900

    feat: AuthentificationService 정의

--- PartyQuest_iOS/PartyQuest_iOS/Network/Authentification/DefaultAuthentificationService.swift
+++ PartyQuest_iOS/PartyQuest_iOS/Network/Authentification/DefaultAuthentificationService.swift
@@ -6,7 +6,13 @@
 //
 
 import Foundation
+import RxSwift
 import Moya
+
+protocol AuthentificationService {
+    func requestLogIn(email: String, password: String) -> Single<UserData>
+    func requestSignUp(email: String, password: String, nickname: String, birth: String) -> Single<UserData>
+}
 
 final class DefaultAuthentificationService: AuthentificationService {
     private let provider: MoyaProvider<AuthentificationAPI>
@@ -15,13 +21,19 @@ final class DefaultAuthentificationService: AuthentificationService {
         self.provider = provider
     }
     
-    func requestLogIn(email: String, password: String) {
-        provider.request(.logIn(email: email, password: password)) { result in
-            <#code#>
-        }
+    func requestLogIn(email: String, password: String) -> Single<UserData> {
+        return provider.rx.request(.logIn(email: email,
+                                          password: password))
+        .filterSuccessfulStatusCodes()
+        .map(UserData.self)
     }
     
-    func requestSignUp(email: String, password: String, nickname: String, birth: String) {
-        <#code#>
+    func requestSignUp(email: String, password: String, nickname: String, birth: String) -> Single<UserData> {
+        return provider.rx.request(.signUp(email: email,
+                                           password: password,
+                                           nickname: nickname,
+                                           birth: birth))
+        .filterSuccessfulStatusCodes()
+        .map(UserData.self)
     }
 }

f3a0c2149befbd1d7405227a17ead2dd4c794d9c
HarryHyeon <hharry0728@gmail.com>
2023-11-14 12:37:42 +0900

    feat: SocialUserData 타입 정의

---
+++ PartyQuest_iOS/PartyQuest_iOS/Domain/Authentification/DTO/SocialUserData.swift
@@ -0,0 +1,15 @@
+//
+//  SocialUserData.swift
+//  PartyQuest_iOS
+//
+//  Created by Harry on 2023/11/14.
+//
+
+import Foundation
+
+struct SocialUserData {
+    var id: String?
+    var email: String?
+    var nickName: String?
+    var platform: String?
+}

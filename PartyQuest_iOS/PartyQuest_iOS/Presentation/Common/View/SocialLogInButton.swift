e0525cc42a948c3db1ce413bf942d8d7f7aa881c
HarryHyeon <hharry0728@gmail.com>
2023-11-21 14:34:45 +0900

    feat: SocialLogInButton 버튼 서브클래스 정의, LogInViewController에 버튼 해당 버튼으로 대체

---
+++ PartyQuest_iOS/PartyQuest_iOS/Presentation/Common/View/SocialLogInButton.swift
@@ -0,0 +1,95 @@
+//
+//  SocialLogInButton.swift
+//  PartyQuest_iOS
+//
+//  Created by Harry on 2023/11/20.
+//
+
+import UIKit
+
+class SocialLogInButton: UIButton {
+    enum Platform {
+        case kakao
+        case apple
+        case google
+        case naver
+        
+        var labelText: String {
+            switch self {
+            case .kakao:
+                return "카카오로 로그인"
+            case .apple:
+                return "애플로 로그인"
+            case .google:
+                return "구글로 로그인"
+            case .naver:
+                return "네이버로 로그인"
+            }
+        }
+        
+        var logoImageName: String {
+            switch self {
+            case .kakao:
+                return "kakaoButton_icon"
+            case .apple:
+                return "appleButton_black_icon"
+            case .google:
+                return "googleButton_icon"
+            case .naver:
+                return "naverButton_icon"
+            }
+        }
+    }
+    
+    private let socialLogoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let socialNameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = .preferredFont(forTextStyle: .caption1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    init(platform: Platform) {
+        super.init(frame: .zero)
+        
+        self.configureUI()
+        self.setConstraints()
+        self.updateLogo(platform)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        addSubview(socialLogoImageView)
+        addSubview(socialNameLabel)
+    }
+    
+    private func updateLogo(_ platform: Platform) {
+        socialLogoImageView.image = UIImage(named: platform.logoImageName)
+        socialNameLabel.text = platform.labelText
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            socialLogoImageView.topAnchor.constraint(equalTo: topAnchor),
+            socialLogoImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            socialLogoImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            socialLogoImageView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.7),
+            socialLogoImageView.widthAnchor.constraint(equalTo: socialLogoImageView.heightAnchor),
+            
+            socialNameLabel.topAnchor.constraint(equalTo: socialLogoImageView.bottomAnchor, constant: 4),
+            socialNameLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+            socialNameLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            socialNameLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+}

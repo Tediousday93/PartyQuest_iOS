fc907f78c4c9e48c63d4d390dc5cc0aa07da5d68
ROWAN <kimkj93@naver.com>
2023-12-05 16:40:37 +0900

    feat: NaverUser, NaverAuthService 타입 정의

--- PartyQuest_iOS/PartyQuest_iOS/SocialAuthentication/Entity/DomainConvertibleType.swift
+++ PartyQuest_iOS/PartyQuest_iOS/SocialAuthentication/Entity/DomainConvertibleType.swift
@@ -29,3 +29,20 @@ extension KakaoSDKUser.User: DomainConvertibleType {
         )
     }
 }
+
+extension NaverUser: DomainConvertibleType {
+    typealias Domain = SocialUserData
+    
+    func toDomain() -> SocialUserData {
+        guard let secrets = Bundle.main.infoDictionary?["SERVICE_SECRETS"] as? String else {
+            fatalError("Can not find Secrets Key")
+        }
+        
+        return SocialUserData(
+            email: email,
+            secrets: secrets,
+            nickName: nickName,
+            platform: .naver
+        )
+    }
+}
---
+++ PartyQuest_iOS/PartyQuest_iOS/SocialAuthentication/Service/NaverAuthService.swift
@@ -0,0 +1,21 @@
+//
+//  NaverAuthService.swift
+//  PartyQuest_iOS
+//
+//  Created by Rowan on 2023/12/05.
+//
+
+import RxSwift
+import RxAlamofire
+import NaverThirdPartyLogin
+
+final class NaverAuthService: SocialAuthService {
+    typealias UserInfo = <#type#>
+    
+    func requestLogIn() -> RxSwift.Observable<Void> {
+        <#code#>
+    }
+    
+    
+    
+}

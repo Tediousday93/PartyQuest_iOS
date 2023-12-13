fd7e50c520722e0cdcb9b09e174cba96406046c9
HarryHyeon <hharry0728@gmail.com>
2023-12-13 16:16:08 +0900

    feat UserProfile 정의

---
+++ PartyQuest_iOS/PartyQuest_iOS/Presentation/Scenes/TabBarScenes/HomeView/UserProfile.swift
@@ -0,0 +1,14 @@
+//
+//  UserProfile.swift
+//  PartyQuest_iOS
+//
+//  Created by Harry on 2023/12/12.
+//
+
+import Foundation
+
+struct UserProfile: Hashable {
+    var imageData: Data?
+    let nickName: String
+    let email: String
+}

e1221157a1dae75022747edc83679e9343e561af
HarryHyeon <hharry0728@gmail.com>
2023-12-13 16:57:13 +0900

    feat: HomeViewModel 추가

---
+++ PartyQuest_iOS/PartyQuest_iOS/Presentation/Scenes/TabBarScenes/HomeView/HomeViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewModel.swift
+//  PartyQuest_iOS
+//
+//  Created by Harry on 2023/12/13.
+//
+
+final class HomeViewModel {
+    private let coordinator: HomeCoordinator
+    
+    init(coordinator: HomeCoordinator) {
+        self.coordinator = coordinator
+    }
+}

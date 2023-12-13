d4ed3ecfbba35356af9bd364ce8ac6d83bae99f1
ROWAN <kimkj93@naver.com>
2023-12-13 15:52:41 +0900

    feat: PartyCell 이니셜라이저, prepareForReuse 정의

--- PartyQuest_iOS/PartyQuest_iOS/Presentation/Common/Components/PartyCell.swift
+++ PartyQuest_iOS/PartyQuest_iOS/Presentation/Common/Components/PartyCell.swift
@@ -23,7 +23,6 @@ final class PartyCell: UICollectionViewCell {
         
         return label
     }()
-    
     private let memberCountLabel: UILabel = {
         let label = UILabel()
         label.textColor = .black
@@ -31,4 +30,71 @@ final class PartyCell: UICollectionViewCell {
         
         return label
     }()
+    private let titleStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        
+        return stackView
+    }()
+    
+    private let todoLabel: LeftImageLabel = LeftImageLabel(imageName: "todo")
+    private let doingLabel: LeftImageLabel = LeftImageLabel(imageName: "doing")
+    private let doneLabel: LeftImageLabel = LeftImageLabel(imageName: "done")
+    private let stateStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        
+        return stackView
+    }()
+    
+    private let partyMasterLabel: LeftImageLabel = LeftImageLabel(imageName: "partyMaster")
+    private let creationDateLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .preferredFont(forTextStyle: .body)
+        
+        return label
+    }()
+    private let partyInfoStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        
+        return stackView
+    }()
+    
+    private let outterStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 8
+        
+        return stackView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        topImageView.image = nil
+        titleLabel.text = nil
+        memberCountLabel.text = nil
+        todoLabel.setText(nil)
+        doingLabel.setText(nil)
+        doneLabel.setText(nil)
+        partyMasterLabel.setText(nil)
+        creationDateLabel.text = nil
+    }
 }

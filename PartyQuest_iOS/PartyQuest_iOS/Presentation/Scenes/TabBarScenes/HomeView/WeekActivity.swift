2560877f10020ff8a9c5925f6de24809076cef6c
HarryHyeon <hharry0728@gmail.com>
2023-12-20 14:40:19 +0900

    feat: WeekActivity 정의

---
+++ PartyQuest_iOS/PartyQuest_iOS/Presentation/Scenes/TabBarScenes/HomeView/WeekActivity.swift
@@ -0,0 +1,15 @@
+//
+//  WeekActivity.swift
+//  PartyQuest_iOS
+//
+//  Created by Harry on 2023/12/19.
+//
+
+import Foundation
+
+struct WeekActivity: Hashable {
+    let questCount: String
+    let completeCount: String
+    let postCount: String
+    let commentCount: String
+}

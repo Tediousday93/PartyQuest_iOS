94bcb1c92957ff0e86c748fbec31af4a892d6122
HarryHyeon <hharry0728@gmail.com>
2023-12-21 15:32:00 +0900

    feat: HomeViewController - 컬렉션뷰에 doingQuest 섹션 추가, HomeViewModel - viewWillAppear 이벤트에 doingQuest 데이터도 같이 방출하도록 추가

--- PartyQuest_iOS/PartyQuest_iOS/Presentation/Scenes/TabBarScenes/HomeView/HomeViewController.swift
+++ PartyQuest_iOS/PartyQuest_iOS/Presentation/Scenes/TabBarScenes/HomeView/HomeViewController.swift
@@ -13,6 +13,7 @@ final class HomeViewController: UIViewController {
     typealias Snapshot = NSDiffableDataSourceSnapshot<Section, AnyHashable>
     typealias UserProfileCellRegistration = UICollectionView.CellRegistration<UserProfileCell, UserProfile>
     typealias WeekActivityCellRegistration = UICollectionView.CellRegistration<WeekActivityCell, WeekActivity>
+    typealias DoingQuestCellRegistration = UICollectionView.CellRegistration<DoingQuestCell, Quest>
     typealias HeaderRegistration = UICollectionView.SupplementaryRegistration<HomeCollectionHeaderView>
     
     private var dataSource: DataSource!
@@ -21,7 +22,7 @@ final class HomeViewController: UIViewController {
         let label = UILabel()
         label.font = .preferredFont(forTextStyle: .headline)
         label.text = "Harry님, 환영합니다!"
-        label.textColor = .black
+        label.textColor = PQColor.text
         label.numberOfLines = 1
         
         return label
@@ -30,7 +31,6 @@ final class HomeViewController: UIViewController {
     private lazy var collectionView: UICollectionView = {
         let colletionView = UICollectionView(frame: .zero,
                                              collectionViewLayout: createCollectionViewLayout())
-        colletionView.backgroundColor = .red
         colletionView.translatesAutoresizingMaskIntoConstraints = false
         
         return colletionView
@@ -97,23 +97,21 @@ final class HomeViewController: UIViewController {
             switch section {
             case 0:
                 let config = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
-                
                 let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                        heightDimension: .absolute(20))
+                                                        heightDimension: .absolute(25))
                 let header = NSCollectionLayoutBoundarySupplementaryItem(
                     layoutSize: headerSize,
                     elementKind: "ProfileHeader",
                     alignment: .top
                 )
                 let listSection = NSCollectionLayoutSection.list(using: config, layoutEnvironment: env)
-                listSection.contentInsets = .init(top: 20, leading: 20, bottom: 20, trailing: 20)
+                listSection.contentInsets = .init(top: 20, leading: 20, bottom: 40, trailing: 20)
                 listSection.boundarySupplementaryItems = [header]
                 
                 
                 return listSection
-            default:
+            case 1:
                 let config = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
-                
                 let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
                                                         heightDimension: .absolute(20))
                 let header = NSCollectionLayoutBoundarySupplementaryItem(
@@ -122,9 +120,26 @@ final class HomeViewController: UIViewController {
                     alignment: .top
                 )
                 let listSection = NSCollectionLayoutSection.list(using: config, layoutEnvironment: env)
-                listSection.contentInsets = .init(top: 15, leading: 20, bottom: 20, trailing: 20)
+                listSection.contentInsets = .init(top: 15, leading: 20, bottom: 35, trailing: 20)
                 listSection.boundarySupplementaryItems = [header]
-
+                
+                return listSection
+                
+            default:
+                var config = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+                config.showsSeparators = false
+                
+                let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                        heightDimension: .absolute(20))
+                let header = NSCollectionLayoutBoundarySupplementaryItem(
+                    layoutSize: headerSize,
+                    elementKind: "DoingQuestHeader",
+                    alignment: .top
+                )
+                let listSection = NSCollectionLayoutSection.list(using: config, layoutEnvironment: env)
+                listSection.contentInsets = .init(top: 15, leading: 20, bottom: 35, trailing: 20)
+                listSection.boundarySupplementaryItems = [header]
+                
                 return listSection
             }
         }
@@ -140,6 +155,10 @@ final class HomeViewController: UIViewController {
             cell.configure(with: weekActivity)
         }
         
+        let doingQuestCellRegistration = DoingQuestCellRegistration { cell, indexPath, doingQuest in
+            cell.configure(with: doingQuest)
+        }
+        
         let profileHeaderRegistration = HeaderRegistration(elementKind: "ProfileHeader") {
             supplementaryView, elementKind, indexPath in
             supplementaryView.setFont(PQFont.subTitle)
@@ -151,6 +170,11 @@ final class HomeViewController: UIViewController {
             supplementaryView.configureTitle(string: "이번주 활동")
         }
         
+        let doingQuestHeaderRegistration = HeaderRegistration(elementKind: "DoingQuestHeader") {
+            supplementaryView, elementKind, indexPath in
+            supplementaryView.configureTitle(string: "진행중인 퀘스트")
+        }
+        
         dataSource = DataSource(collectionView: collectionView) { collectionView, indexPath, item in
             if let profile = item as? UserProfile {
                 return collectionView.dequeueConfiguredReusableCell(using: userProfileCellRegistration,
@@ -160,6 +184,10 @@ final class HomeViewController: UIViewController {
                 return collectionView.dequeueConfiguredReusableCell(using: weekActivityCellRegistration,
                                                                     for: indexPath,
                                                                     item: activity)
+            } else if let quest = item as? Quest {
+                return collectionView.dequeueConfiguredReusableCell(using: doingQuestCellRegistration,
+                                                                    for: indexPath,
+                                                                    item: quest)
             }
             
             return UICollectionViewCell()
@@ -176,18 +204,24 @@ final class HomeViewController: UIViewController {
                     using: activityHeaderRegistration,
                     for: indexPath
                 )
+            } else if elementKind == "DoingQuestHeader" {
+                return collectionView.dequeueConfiguredReusableSupplementary(
+                    using: doingQuestHeaderRegistration,
+                    for: indexPath
+                )
             }
             
             return nil
         }
     }
     
-    private func applySnapshot(profiles: [UserProfile], activities: [WeekActivity]) {
+    private func applySnapshot(profiles: [UserProfile], activities: [WeekActivity], doingQuests: [Quest]) {
         var snapshot = Snapshot()
         
-        snapshot.appendSections([.userProfile, .weekActivity])
+        snapshot.appendSections([.userProfile, .weekActivity, .doingQuest])
         snapshot.appendItems(profiles, toSection: .userProfile)
         snapshot.appendItems(activities, toSection: .weekActivity)
+        snapshot.appendItems(doingQuests, toSection: .doingQuest)
         
         self.dataSource.apply(snapshot)
     }
@@ -203,7 +237,9 @@ final class HomeViewController: UIViewController {
         output.homeItems
             .withUnretained(self)
             .subscribe { owner, items in
-                owner.applySnapshot(profiles: [items.userProfile], activities: [items.weekActivity])
+                owner.applySnapshot(profiles: [items.userProfile],
+                                    activities: [items.weekActivity],
+                                    doingQuests: items.doingQuests)
             }
             .disposed(by: disposeBag)
     }
@@ -212,4 +248,5 @@ final class HomeViewController: UIViewController {
 enum Section {
     case userProfile
     case weekActivity
+    case doingQuest
 }
--- PartyQuest_iOS/PartyQuest_iOS/Presentation/Scenes/TabBarScenes/HomeView/HomeViewModel.swift
+++ PartyQuest_iOS/PartyQuest_iOS/Presentation/Scenes/TabBarScenes/HomeView/HomeViewModel.swift
@@ -21,7 +21,9 @@ extension HomeViewModel: ViewModelType {
     }
     
     struct Output {
-        let homeItems: Observable<(userProfile: UserProfile, weekActivity: WeekActivity)>
+        let homeItems: Observable<(userProfile: UserProfile,
+                                   weekActivity: WeekActivity,
+                                   doingQuests: [Quest])>
     }
     
     func transform(_ input: Input) -> Output {
@@ -40,8 +42,33 @@ extension HomeViewModel: ViewModelType {
                              commentCount: 7)
             }
         
-        let homeItems = Observable.combineLatest(userProfile, weekActivity) { userProfile, weekActivity in
-            return (userProfile: userProfile, weekActivity: weekActivity)
+        let doingQuests = input.viewWillAppearEvent
+            .map { _ in
+                [
+                    Quest(title: "백준 385번 풀기",
+                          partyName: "알고리즘 스터디",
+                          status: .doing,
+                          dDay: "2",
+                          partyImageUrl: "house.fill"),
+                    
+                    Quest(title: "1,2,3번 연습하기",
+                          partyName: "iOS 면접 스터디",
+                          status: .doing,
+                          dDay: "3",
+                          partyImageUrl: "house"),
+                    
+                    Quest(title: "개인책 20쪽 읽기",
+                          partyName: "독서파티",
+                          status: .doing,
+                          dDay: "3",
+                          partyImageUrl: "house")
+                ]
+            }
+        
+        let homeItems = Observable.combineLatest(userProfile, weekActivity, doingQuests) {
+            userProfile, weekActivity, doingQuests in
+            
+            return (userProfile: userProfile, weekActivity: weekActivity, doingQuests: doingQuests)
         }
         
         return Output(homeItems: homeItems)

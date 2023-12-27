a115de42137cee517bd090d9b8c30e16d2db7db5
ROWAN <kimkj93@naver.com>
2023-12-27 15:43:39 +0900

    refactor: Coordinator 인터페이스 이름 수정 및 추가, extension 기본구현 추가

--- PartyQuest_iOS/PartyQuest_iOS/Presentation/Common/Coordinator/Coordinator.swift
+++ PartyQuest_iOS/PartyQuest_iOS/Presentation/Common/Coordinator/Coordinator.swift
@@ -10,8 +10,28 @@ import UIKit
 protocol Coordinator: AnyObject {
     var navigationController: UINavigationController? { get set }
     var parentCoordinator: Coordinator? { get set }
+    var childCoordinators: [Coordinator] { get set }
     
     func start()
-    func start(coordinator: Coordinator)
-    func didFinish(coordinator: Coordinator)
+    func finish()
+    func start(child: Coordinator)
+    func finish(child: Coordinator)
+}
+
+extension Coordinator {
+    func finish() {
+        parentCoordinator?.finish(child: self)
+    }
+    
+    func start(child: Coordinator) {
+        childCoordinators.append(child)
+        child.parentCoordinator = self
+        child.start()
+    }
+    
+    func finish(child: Coordinator) {
+        if let index = childCoordinators.firstIndex(where: { $0 === child }) {
+            childCoordinators.remove(at: index)
+        }
+    }
 }

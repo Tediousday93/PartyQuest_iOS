0c498edb4b93c664329b1ee0c9cc4fd7a25d3726
ROWAN <kimkj93@naver.com>
2024-01-01 18:32:24 +0900

    feat: PartySearchViewController - party card collection view 프로퍼티 및 constraints, bindings 추가
    오토레이아웃, 데이터 바인딩 코드 추가
    각 UIComponent들의 세부 설정 추가

--- PartyQuest_iOS/PartyQuest_iOS/Presentation/Scenes/TabBarScenes/PartySearchView/PartySearchViewController.swift
+++ PartyQuest_iOS/PartyQuest_iOS/Presentation/Scenes/TabBarScenes/PartySearchView/PartySearchViewController.swift
@@ -14,6 +14,7 @@ final class PartySearchViewController: UIViewController {
         let menuItems = ["파티 이름", "파티장"]
         let button = DropDownButton(menuItems: menuItems, menuHeight: 100)
         button.setButton(title: "검색기준")
+        button.setTitleInsets(top: 0, leading: 10, bottom: 0, trailing: 0)
         button.translatesAutoresizingMaskIntoConstraints = false
         
         return button
@@ -21,17 +22,57 @@ final class PartySearchViewController: UIViewController {
     
     private let searchTextField: UITextField = {
         let textField = UITextField()
+        
         let imageView = UIImageView(image: UIImage(systemName: "magnifyingglass"))
-        textField.leftView = imageView
+        imageView.tintColor = .placeholderText
+        
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.layoutMargins = .init(top: 0, left: 8, bottom: 0, right: 8)
+        stackView.addArrangedSubview(imageView)
+        
+        textField.leftView = stackView
+        textField.leftViewMode = .always
         textField.placeholder = "Search Party"
         textField.backgroundColor = .systemGray5
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.font = .preferredFont(forTextStyle: .body)
+        textField.layer.cornerRadius = 10
         
         return textField
     }()
     
+    private let collectionViewController: PartyCardCollectionViewController = .init()
+    
+    private let viewModel: PartySearchViewModel
+    private var disposeBag: DisposeBag
+    
+    init(viewModel: PartySearchViewModel) {
+        self.viewModel = viewModel
+        self.disposeBag = .init()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        self.disposeBag = .init()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureNavigationBar()
+        configureRootView()
+        setSubviews()
+        setConstraints()
+        setBindings()
+    }
+    
+    private func configureNavigationBar() {
+        title = "파티 찾기"
     }
     
     private func configureRootView() {
@@ -39,11 +80,41 @@ final class PartySearchViewController: UIViewController {
     }
     
     private func setSubviews() {
+        view.addSubview(searchTextField)
+        view.addSubview(collectionViewController.view)
         view.addSubview(dropDownButton)
     }
     
     private func setConstraints() {
-        
+        NSLayoutConstraint.activate([
+            dropDownButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            dropDownButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: PQSpacing.side),
+            dropDownButton.trailingAnchor.constraint(equalTo: searchTextField.leadingAnchor, constant: -5),
+            dropDownButton.heightAnchor.constraint(equalToConstant: 150),
+            dropDownButton.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.26),
+            
+            searchTextField.topAnchor.constraint(equalTo: dropDownButton.topAnchor),
+            searchTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -PQSpacing.side),
+            searchTextField.heightAnchor.constraint(equalToConstant: 50),
+            
+            collectionViewController.view.topAnchor.constraint(equalTo: searchTextField.bottomAnchor, constant: 15),
+            collectionViewController.view.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionViewController.view.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionViewController.view.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
-
+    
+    private func setBindings() {
+        let viewWillAppearEvent = rx.sentMessage(#selector(PartyListViewController.viewWillAppear))
+            .map { _ in}
+        let input = PartySearchViewModel.Input(
+            viewWillAppearEvent: viewWillAppearEvent
+        )
+        
+        let output = viewModel.transform(input)
+        
+        output.partyItems
+            .bind(to: collectionViewController.items)
+            .disposed(by: disposeBag)
+    }
 }
--- PartyQuest_iOS/PartyQuest_iOS/Presentation/Scenes/TabBarScenes/PartySearchView/PartySearchViewModel.swift
+++ PartyQuest_iOS/PartyQuest_iOS/Presentation/Scenes/TabBarScenes/PartySearchView/PartySearchViewModel.swift
@@ -15,14 +15,38 @@ final class PartySearchViewModel {
 
 extension PartySearchViewModel: ViewModelType {
     struct Input {
-        
+        let viewWillAppearEvent: Observable<Void>
     }
     
     struct Output {
-        
+        let partyItems: Observable<[PartyItem]>
     }
     
     func transform(_ input: Input) -> Output {
-        return Output()
+        let partyItems = input.viewWillAppearEvent
+            .map { _ in
+                [
+                    PartyItem(topImage: UIImage(named: "party_card_image"),
+                              title: "알고리즘 스터디",
+                              memberCount: "5/10",
+                              todoQuestCount: "2",
+                              doingQuestCount: "1",
+                              doneQuestCount: "1",
+                              partyMaster: "Harry",
+                              creationDate: "생성일 2023.10.01"),
+                    PartyItem(topImage: UIImage(named: "party_card_image"),
+                              title: "독서파티",
+                              memberCount: "5/10",
+                              todoQuestCount: "2",
+                              doingQuestCount: "1",
+                              doneQuestCount: "1",
+                              partyMaster: "Harry",
+                              creationDate: "생성일 2023.10.02"),
+                ]
+            }
+        
+        return Output(
+            partyItems: partyItems
+        )
     }
 }

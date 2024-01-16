a67fe180ab0c3cfc4bc5f2dbdb970714b7e7b253
HarryHyeon <hharry0728@gmail.com>
2024-01-16 17:14:25 +0900

    refactor: UserData - platform 옵셔널로 변경

--- PartyQuest_iOS/PartyQuest_iOS/Domain/Authentication/Entries/UserData.swift
+++ PartyQuest_iOS/PartyQuest_iOS/Domain/Authentication/Entries/UserData.swift
@@ -9,14 +9,12 @@ struct UserData {
     var email: String?
     var secrets: String?
     var nickName: String?
-    var platform: LogInPlatform
+    var platform: LogInPlatform?
     var serviceToken: ServiceToken?
 }
 
 enum LogInPlatform {
-    case partyQuest
     case kakao
-    case apple
     case google
     case naver
 }

90b8168ea2b2f18b40fa2cd7aec180e00a608f78
ROWAN <kimkj93@naver.com>
2024-01-23 14:37:29 +0900

    feat: PartyInfoViewController 타입 생성

---
+++ PartyQuest_iOS/PartyQuest_iOS/Presentation/Scenes/TabBarScenes/PartyInfoView/PartyInfoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PartyInfoViewController.swift
+//  PartyQuest_iOS
+//
+//  Created by Rowan on 2024/01/23.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class PartyInfoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

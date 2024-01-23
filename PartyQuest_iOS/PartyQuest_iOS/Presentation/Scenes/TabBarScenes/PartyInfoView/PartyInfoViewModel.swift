b8d1df72e46e1eed85e88416be1951a75daf447c
ROWAN <kimkj93@naver.com>
2024-01-23 14:39:14 +0900

    feat: PartyInfoViewModel 타입 정의

---
+++ PartyQuest_iOS/PartyQuest_iOS/Presentation/Scenes/TabBarScenes/PartyInfoView/PartyInfoViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  PartyInfoViewModel.swift
+//  PartyQuest_iOS
+//
+//  Created by Rowan on 2024/01/23.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class PartyInfoViewModel {
+    
+}
+
+extension PartyInfoViewModel: ViewModelType {
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(_ input: Input) -> Output {
+        return Output()
+    }
+}

a4fb30f80d1873cba9fe9b9d9a10a53e1befd5a0
ROWAN <kimkj93@naver.com>
2024-02-02 16:13:56 +0900

    refactor: ModifyingListViewController 불필요한 코드 제거 및 컨벤션 수정, collectionview 기본 설정 추가
    불필요한 RxSwift 코드 삭제
    코드 컨벤션 수정 - 메서드 위치 호출 순서대로 변경 (configureDataSource)

--- PartyQuest_iOS/PartyQuest_iOS/Presentation/Components/ModifiyingListView/ModifyingListViewController.swift
+++ PartyQuest_iOS/PartyQuest_iOS/Presentation/Components/ModifiyingListView/ModifyingListViewController.swift
@@ -6,8 +6,6 @@
 //
 
 import UIKit
-import RxSwift
-import RxCocoa
 
 final class ModifyingListViewController: UIViewController {
     private enum Section {
@@ -21,16 +19,6 @@ final class ModifyingListViewController: UIViewController {
     private let collectionView: UICollectionView = .init(frame: .zero, collectionViewLayout: .init())
     
     private var dataSource: DataSource!
-    private var disposeBag: DisposeBag
-    
-    init() {
-        self.disposeBag = .init()
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,8 +33,25 @@ final class ModifyingListViewController: UIViewController {
     }
     
     private func configureCollectionView() {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .white
+        
         setCollectionViewLayout()
         configureDataSource()
+    }
+    
+    private func setCollectionViewLayout() {
+        let layout = UICollectionViewCompositionalLayout { section, environment in
+            let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+            let section = NSCollectionLayoutSection.list(
+                using: configuration,
+                layoutEnvironment: environment
+            )
+            section.contentInsets = .init(top: 16, leading: 16, bottom: 16, trailing: 16)
+            return section
+        }
+        
+        collectionView.setCollectionViewLayout(layout, animated: false)
     }
     
     private func configureDataSource() {
@@ -62,20 +67,6 @@ final class ModifyingListViewController: UIViewController {
             )
             return cell
         }
-    }
-    
-    private func setCollectionViewLayout() {
-        let layout = UICollectionViewCompositionalLayout { section, environment in
-            let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
-            let section = NSCollectionLayoutSection.list(
-                using: configuration,
-                layoutEnvironment: environment
-            )
-            section.contentInsets = .init(top: 16, leading: 16, bottom: 16, trailing: 16)
-            return section
-        }
-        
-        collectionView.setCollectionViewLayout(layout, animated: false)
     }
     
     private func setSubviews() {

e17cea4fcbf3fe1e9eb08c71ad10b087910592bf
ROWAN <kimkj93@naver.com>
2024-02-14 14:22:12 +0900

    refactor: ModifyingListCell 불필요한 프로퍼티, 메서드 제거 및 접근제어 수정
    상위 뷰 컨트롤러에서 view model과 바인딩을 위해 modifyButton 접근제어를 internal로 변경
    불필요한 프로퍼티 modifyingItem, collectionView 제거
    불필요한 버튼 액션 메서드 onModifyButtonTap 제거

--- PartyQuest_iOS/PartyQuest_iOS/Presentation/Components/ModifiyingListView/ModifyingListCell.swift
+++ PartyQuest_iOS/PartyQuest_iOS/Presentation/Components/ModifiyingListView/ModifyingListCell.swift
@@ -34,7 +34,7 @@ final class ModifyingListCell: UICollectionViewListCell {
         return stackView
     }()
     
-    private let modifyButton: UIButton = {
+    let modifyButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("수정", for: .normal)
         button.tintColor = .white
@@ -42,15 +42,6 @@ final class ModifyingListCell: UICollectionViewListCell {
         
         return button
     }()
-    
-    private(set) var modifyingItem: ModifyingItem? {
-        didSet(newItem) {
-            titleLabel.text = newItem?.title
-            valueLabel.text = newItem?.value
-        }
-    }
-    
-    private weak var collectionView: UICollectionView?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -95,23 +86,11 @@ final class ModifyingListCell: UICollectionViewListCell {
         
         self.accessories = [trailingModifyButton]
     }
-    
-    @objc
-    private func onModifyButtonTap() {
-        if let collectionView,
-           let indexPath = collectionView.indexPath(for: self) {
-            collectionView.selectItem(at: indexPath, animated: true, scrollPosition: .centeredHorizontally)
-            collectionView.delegate?.collectionView?(collectionView, didSelectItemAt: indexPath)
-        }
-    }
 }
 
 extension ModifyingListCell {
-    func configure(with item: ModifyingItem, collectionView: UICollectionView?) {
-        self.modifyingItem = item
-        self.collectionView = collectionView
-        self.modifyButton.addTarget(self,
-                                    action: #selector(onModifyButtonTap),
-                                    for: .touchUpInside)
+    func configure(with item: ModifyingItem) {
+        titleLabel.text = item.title
+        valueLabel.text = item.value
     }
 }

069475d808e09491707e3051391f920498dab6c9
HarryHyeon <hharry0728@gmail.com>
2024-02-15 16:38:34 +0900

    refactor: DomainConvertibleType - toDomain() throws 메서드로 변경, return 값 UserData -> SocialUser로 변경

--- PartyQuest_iOS/PartyQuest_iOS/SocialAuthentication/DTO/DomainConvertibleType.swift
+++ PartyQuest_iOS/PartyQuest_iOS/SocialAuthentication/DTO/DomainConvertibleType.swift
@@ -8,59 +8,72 @@
 import KakaoSDKUser
 import GoogleSignIn
 
+enum DomainConvertError: Error {
+    case nilSocialUserData
+}
+
 protocol DomainConvertibleType {
     associatedtype Domain
     
-    func toDomain() -> Domain
+    func toDomain() throws -> Domain
 }
 
 extension KakaoSDKUser.User: DomainConvertibleType {
-    typealias Domain = UserData
+    typealias Domain = SocialUser
     
-    func toDomain() -> UserData {
+    func toDomain() throws -> SocialUser {
         guard let secrets = Bundle.main.infoDictionary?["SERVICE_SECRETS"] as? String else {
             fatalError("Can not find Secrets Key")
         }
         
-        return UserData(
-            email: kakaoAccount?.email,
-            secrets: secrets,
-            nickName: properties?["nickname"],
-            platform: .kakao
+        guard let email = kakaoAccount?.email,
+              let nickname = properties?["nickname"] else {
+            throw DomainConvertError.nilSocialUserData
+        }
+        
+        return SocialUser(
+            email: email,
+            nickname: nickname
         )
     }
 }
 
 extension GIDGoogleUser: DomainConvertibleType {
-    typealias Domain = UserData
+    typealias Domain = SocialUser
     
-    func toDomain() -> UserData {
+    func toDomain() throws -> SocialUser {
         guard let secrets = Bundle.main.infoDictionary?["SERVICE_SECRETS"] as? String else {
             fatalError("Can not find Secrets Key ")
         }
         
-        return UserData(
-            email: profile?.email,
-            secrets: secrets,
-            nickName: profile?.name,
-            platform: .google
+        guard let email = profile?.email,
+              let nickname = profile?.name else {
+            throw DomainConvertError.nilSocialUserData
+        }
+        
+        return SocialUser(
+            email: email,
+            nickname: nickname
         )
     }
 }
 
 extension NaverUserData: DomainConvertibleType {
-    typealias Domain = UserData
+    typealias Domain = SocialUser
     
-    func toDomain() -> UserData {
+    func toDomain() throws -> SocialUser {
         guard let secrets = Bundle.main.infoDictionary?["SERVICE_SECRETS"] as? String else {
             fatalError("Can not find Secrets Key")
         }
         
-        return UserData(
+        guard let email = email,
+              let nickname = nickname else {
+            throw DomainConvertError.nilSocialUserData
+        }
+        
+        return SocialUser(
             email: email,
-            secrets: secrets,
-            nickName: nickName,
-            platform: .naver
+            nickname: nickname
         )
     }
 }
